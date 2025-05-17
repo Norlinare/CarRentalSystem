0ccd4b716780ad37e63d7fbd6e931ce9ece2d5df
Imir <imir.norlin96@hotmail.com>
2025-05-17 16:55:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentalSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
